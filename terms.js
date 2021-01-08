e381691601a1fc5bc751ba184500b914fb93a3df
Gerald Bivens <geraldbivens@gmail.com>
2021-01-08 11:37:03 -0700

    Render terms.

--- terms.js
+++ terms.js
@@ -2,4 +2,12 @@ const termsList = document.querySelector('#terms-list')
 
 fetch('http://localhost:3000/terms')
     .then(response => response.json())
-    .then(result => console.log('terms', result))
+    .then(renderTerms)
+
+function renderTerms(terms){
+    terms.forEach(term => {
+        const name = document.createElement('li')
+        name.innerText = term.name
+        termsList.append(name)
+    })   
+}
